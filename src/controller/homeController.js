930426a16b3b3b42774c973fbc506bc39fcd496e
Tong Hinh <hinhvt1998@gmail.com>
2022-09-19 00:06:27 +0700

    upload single file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,7 @@
 import { json } from 'body-parser';
 import pool from '../configs/connectDB';
+import multer from 'multer';
+import path from 'path';
 // Get home page
 let getHomePage = async (req, res) => {
     // Xu ly code logic o controller
@@ -44,6 +46,28 @@ let updatePage = async (req, res) => {
 
 }
 
+
+
+let getUploadFilePage = async (req, res) => {
+    return res.render('uploadFile.ejs')
+}
+
+
+let handleUploadFile = async (req, res) => {
+
+    if (req.fileValidationError) {
+
+        return res.send(req.fileValidationError);
+    }
+    else if (!req.file) {
+        return res.send('Please select an image to upload');
+    }
+
+    // Display uploaded image for user validation
+    res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+    // });
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getDetailPage: getDetailPage,
@@ -51,4 +75,6 @@ module.exports = {
     deleteUser: deleteUser,
     getEditPage: getEditPage,
     updatePage: updatePage,
+    getUploadFilePage: getUploadFilePage,
+    handleUploadFile: handleUploadFile,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -1,10 +1,37 @@
 import express from 'express';
+import multer from 'multer';
+import path from 'path';
+var appRoot = require('app-root-path');
+let router = express.Router();
 
 // Controller
 import homeController from '../controller/homeController';
 
 // Khai bao routter de su dung ham router cua express
-let router = express.Router();
+
+// Upload file
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, appRoot + "/src/public/image/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
+
 
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
@@ -13,6 +40,9 @@ const initWebRoute = (app) => {
     router.post('/delete-user', homeController.deleteUser);
     router.get('/edit-user/:id', homeController.getEditPage);
     router.post('/update-user', homeController.updatePage);
+
+    router.get('/upload', homeController.getUploadFilePage);
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile);
 
 
     return app.use('/', router)
